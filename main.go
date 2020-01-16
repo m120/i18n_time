5dc549f192e0c7cd7460f36cb971c7786a478f50
mpg-mitsuo-hori <mitsuo.hori+github@medpeer.co.jp>
2020-01-16 16:51:27 +0900

    main go update

--- main.go
+++ main.go
@@ -1,9 +1,21 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
+	"os"
 	"time"
 )
+
+type tzdata struct {
+	REGION      string `json:"region"`
+	ZONE1       string `json:"zone_1"`
+	ZONE2       string `json:"zone_2"`
+	CODE        string `json:"code"`
+	TZ          string `json:"tz"`
+	COORDINATES string `json:"coordinates"`
+}
 
 func main() {
 	now := time.Now()
@@ -12,8 +24,19 @@ func main() {
 	// Local
 	fmt.Println(now.Format(timeformat), "\t:", now.Location())
 
-	for _, l := range []string{"Pacific/Auckland", "Asia/Tokyo", "US/Central"} {
-		loc, _ := time.LoadLocation(l)
+	// json read
+	raw, err := ioutil.ReadFile("./tz.json")
+	if err != nil {
+		fmt.Println(err.Error())
+		os.Exit(1)
+	}
+
+	var tzd []tzdata
+	json.Unmarshal(raw, &tzd)
+
+	for _, tzs := range tzd {
+		//loc, _ := time.LoadLocation(tz.REGION + "/" + tz.ZONE1)
+		loc, _ := time.LoadLocation(tzs.TZ)
 		nowloc := now.In(loc)
 		fmt.Println(nowloc.Format(timeformat), "\t:", loc)
 	}
